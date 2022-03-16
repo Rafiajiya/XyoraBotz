38b3b87a17562ed534561b0b1b5e094fca54f042
Rafiajiya <94803405+Rafiajiya@users.noreply.github.com>
2022-03-16 12:58:32 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -1,17 +1,17 @@
 module.exports.mess = {
 	        wait: '⏰ Perintah Di Laksanakan....',
 			success: '✔️ Selesai Kak',
-			wrongFormat: 'Format salah, coba liat lagi di menu',
+			wrongFormat: 'Format salah Goblok Tolol Bed Lu, coba liat lagi di menu',
 			error: {
 				api: 'Emror Bg',
 				stick: 'ITU BUKAN STIKER TOLOL',
 				Iv: 'JANGAN NGASIH LINK GAJE TOD'
 			},
 			only: {
-				group: '😇 Only Grup Bruh...',
-				admin: '😇 Only Admin Bruh....',
-				premium: '😇 Only Premium Bruh....',
-				owner: '😇 Only Owner Bruh...',
-				Badmin: 'BOT HARUS JADI ADMIN BRO',
+				group: '😇 Lu Bukan Admin Jangan Bnyk Tingkah...',
+				admin: '😇 Lu Bukan Admin Jangan Bnyk Tingkah....',
+				premium: '😇 Beli Premium dulu kak😁....',
+				owner: '😇 Lu Jngn Bnyk Tingkah ...',
+				Badmin: 'BOT HARUS JADI ADMIN Dulu Ngtd',
 			}
 		}
